68f0d129444d28756a8cf8d10d0a057e710ba367
jonany <jonany>
2024-01-15 21:17:47 -0500

    added a config path flag

--- cmd/podstache/podstache.go
+++ cmd/podstache/podstache.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+
 	"github.com/jonany/podstache/v2/cmd/podstache/download"
 	"github.com/spf13/viper"
 )
 
 func main() {
-	viper.SetConfigFile("/home/jonany/src/podstache/cmd/podstache/.env")
-	viper.ReadInConfig()
+	var configFile string
+	flag.StringVar(&configFile, "config", ".env", "path to config file")
+	flag.Parse()
 
-	download.Download(download.DownloadOptions{
-		FeedFilePath:        viper.GetString("FEED_FILE_PATH"),
-		DownloadPath:        viper.GetString("DOWNLOAD_PATH"),
-		FeedLimit:           viper.GetInt("FEED_LIMIT"),
-		ItemLimit:           viper.GetInt("ITEM_LIMIT"),
-		DownloadWorkerLimit: viper.GetInt("DOWNLOAD_WORKER_LIMIT"),
-	})
+	viper.SetConfigFile(configFile)
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Printf("Unable to read in config file: %s\n", configFile)
+	} else {
+		fmt.Printf("Found config file: %s\n", configFile)
+		download.Download(download.DownloadOptions{
+			FeedFilePath:        viper.GetString("FEED_FILE_PATH"),
+			DownloadPath:        viper.GetString("DOWNLOAD_PATH"),
+			FeedLimit:           viper.GetInt("FEED_LIMIT"),
+			ItemLimit:           viper.GetInt("ITEM_LIMIT"),
+			DownloadWorkerLimit: viper.GetInt("DOWNLOAD_WORKER_LIMIT"),
+		})
+	}
 }

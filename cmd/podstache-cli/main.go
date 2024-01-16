dff7d69f7bbea01cd17735f6faa1b21abe46ed2a
jonany <jonany>
2024-01-15 20:45:07 -0500

    experimenting with cobra

--- cmd/podstache-cli/main.go
+++ cmd/podstache-cli/main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"github.com/jonany/podstache/v2/cmd/podstache-cli/cmd"
-	"github.com/jonany/podstache/v2/pkg/podstache"
+	//"github.com/jonany/podstache/v2/pkg/podstache"
 )
 
 func main() {
 	cmd.Execute()
 
-	podstache.Download(podstache.DownloadOptions{
-		FeedFilePath:       "/home/jonany/src/pddl/pods/feeds.opml",
-		DownloadPath:       "/home/jonany/src/podstache/pods",
-		FeedLimit:          1,
-		ItemLimit:          5,
-		MaxDownloadWorkers: 4,
-	})
+	// podstache.Download(podstache.DownloadOptions{
+	// 	FeedFilePath:       "/home/jonany/src/pddl/pods/feeds.opml",
+	// 	DownloadPath:       "/home/jonany/src/podstache/pods",
+	// 	FeedLimit:          1,
+	// 	ItemLimit:          5,
+	// 	MaxDownloadWorkers: 4,
+	// })
 }
